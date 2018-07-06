6aa15376cb084f473b6030bc42485818eea6485a
夜无雪 <37978945+yewuxue@users.noreply.github.com>
2018-07-06 10:26:43 +0800

    Update baidu_search_page_case.py

--- FirstDemo/case/baidu_search_page_case.py
+++ FirstDemo/case/baidu_search_page_case.py
@@ -12,25 +12,25 @@ data = read_Excel(filePath, sheetName)
 testData = data.dict_data()
 print(testData)
 
-@ddt.ddt
+#@ddt.ddt
 class baidu_search_page_case(baseCase):
 
-    # def test_search_baidu01(self):
-    #     self.searchDriver = baidu_search_page_business(self.driver)
-    #     self.searchDriver.baiduSearch("python selenium")
+     def test_search_baidu01(self):
+         self.searchDriver = baidu_search_page_business(self.driver)
+         self.searchDriver.baiduSearch("python selenium")
 
-    def search_baidu02(self,vaule):
-        self.searchDriver=baidu_search_page_business(self.driver)
-        self.searchDriver.baiduSearch(vaule)
+#     def search_baidu02(self,vaule):
+#         self.searchDriver=baidu_search_page_business(self.driver)
+#         self.searchDriver.baiduSearch(vaule)
 
-    @ddt.data(*testData)
-    def test_search_baidu02(self, data):
-        '''百度搜索案例'''
-        print("当前测试数据%s" % data)
+#     @ddt.data(*testData)
+#     def test_search_baidu02(self, data):
+#         '''百度搜索案例'''
+#         print("当前测试数据%s" % data)
 
-        # 调用登录方法
-        self.search_baidu02(data["value"])
-        self.logger.info("调试成功")
+#         # 调用登录方法
+#         self.search_baidu02(data["value"])
+#         self.logger.info("调试成功")
 
 
 
